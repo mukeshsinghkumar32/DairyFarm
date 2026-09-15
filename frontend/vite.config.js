2b5a662178bd17ff3f84bf2da40155dccb6cc8de
mukesh singh <mukesh.singh@bakuun.com>
2026-09-15 20:20:27 +0530

    changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,26 +1,25 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
+      "/api": {
+        target: "https://dairyfarm-backend-7ie0.onrender.com/",
         changeOrigin: true,
       },
       // Laravel storage (keep for compatibility)
-      '/storage': {
-        target: 'http://127.0.0.1:8000',
+      "/storage": {
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
       // Node.js uploads
-      '/uploads': {
-        target: 'http://127.0.0.1:8000',
+      "/uploads": {
+        target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
     },
   },
 });
-
